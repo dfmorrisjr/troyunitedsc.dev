ac06a989384a6868c9e7a5f44d4a22e512351a12
Don Morris <dfmorrisjr@globalsolutionsdev.com>
2013-02-22 08:29:23 -0600

    Added MYSA card to join page

--- join/join.php
+++ join/join.php
@@ -73,6 +73,7 @@
                         <li>League Fees
                         	<ul>
                             	<li>SLYSA: $120-150 est. Spring 2013</li>
+                                <li>MYSA Seasonal Player Card, required by all players and coaches: $20 per season</li>
                             </ul>
                         	
                         </li>
